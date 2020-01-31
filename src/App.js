6070d8a97e0b50ee61effea25fe33bac590ca977
Owen Simon <osimon922@gmail.com>
2020-01-30 23:46:40 -0500

    started doing things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mafia Helper</h1>
+        <div>
+          <Button>Create Game</Button>
+          <Form> 
+          Join Game
+            <input type="text"></input>
+             </Form>
+        </div>
       </header>
     </div>
   );
